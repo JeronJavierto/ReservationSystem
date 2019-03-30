baccc065d3924b6904350c94346946d1f4ab9518
JekRosal <34346272+JekRosal@users.noreply.github.com>
2019-03-30 15:39:26 +0800

    added codes for editing the profile

--- ReservationSystemV2/Databases/itproject.sql
+++ ReservationSystemV2/Databases/itproject.sql
@@ -2,10 +2,10 @@
 -- version 4.8.4
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1:3306
--- Generation Time: Mar 30, 2019 at 06:46 AM
--- Server version: 5.7.24
--- PHP Version: 7.2.14
+-- Host: 127.0.0.1
+-- Generation Time: Mar 30, 2019 at 02:59 AM
+-- Server version: 10.1.37-MariaDB
+-- PHP Version: 7.3.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -28,19 +28,24 @@ SET time_zone = "+00:00";
 -- Table structure for table `client`
 --
 
-DROP TABLE IF EXISTS `client`;
-CREATE TABLE IF NOT EXISTS `client` (
-  `ClientID` int(50) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `client` (
+  `ClientID` int(50) NOT NULL,
   `First_name` varchar(25) NOT NULL,
   `Last_name` varchar(25) NOT NULL,
   `email` varchar(35) NOT NULL,
   `Organization` varchar(25) NOT NULL,
   `position` varchar(25) NOT NULL,
   `password` varchar(50) NOT NULL,
-  `Status` varchar(10) NOT NULL,
-  PRIMARY KEY (`ClientID`),
-  UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=60 DEFAULT CHARSET=latin1;
+  `Status` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `client`
+--
+
+INSERT INTO `client` (`ClientID`, `First_name`, `Last_name`, `email`, `Organization`, `position`, `password`, `Status`) VALUES
+(58, 'jeron', 'javierto', 'jeron@gmail.com', 'PESO', 'President', '123', ''),
+(59, 'jeron', 'javierto', 'jeron@gmail.com', 'PESO', 'President', 'qwe', '');
 
 -- --------------------------------------------------------
 
@@ -48,17 +53,15 @@ CREATE TABLE IF NOT EXISTS `client` (
 -- Table structure for table `event`
 --
 
-DROP TABLE IF EXISTS `event`;
-CREATE TABLE IF NOT EXISTS `event` (
-  `eventID` int(100) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `event` (
+  `eventID` int(100) NOT NULL,
   `Event_name` varchar(50) NOT NULL,
   `Facility` varchar(50) NOT NULL,
   `Date` date NOT NULL,
   `Time_start` time(4) NOT NULL,
   `Time_end` time(4) NOT NULL,
-  `Description` varchar(50) NOT NULL,
-  PRIMARY KEY (`eventID`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+  `Description` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `event`
@@ -74,17 +77,15 @@ INSERT INTO `event` (`eventID`, `Event_name`, `Facility`, `Date`, `Time_start`, 
 -- Table structure for table `facility`
 --
 
-DROP TABLE IF EXISTS `facility`;
-CREATE TABLE IF NOT EXISTS `facility` (
-  `FacID` int(255) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `facility` (
+  `FacID` int(255) NOT NULL,
   `Level` varchar(255) NOT NULL,
   `room` varchar(255) NOT NULL,
   `roomType` varchar(255) NOT NULL,
   `description` varchar(255) NOT NULL,
   `capacity` int(255) NOT NULL,
-  `Reservation` varchar(255) NOT NULL,
-  PRIMARY KEY (`FacID`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+  `Reservation` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `facility`
@@ -100,17 +101,15 @@ INSERT INTO `facility` (`FacID`, `Level`, `room`, `roomType`, `description`, `ca
 -- Table structure for table `reservation`
 --
 
-DROP TABLE IF EXISTS `reservation`;
-CREATE TABLE IF NOT EXISTS `reservation` (
-  `resID` int(255) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `reservation` (
+  `resID` int(255) NOT NULL,
   `DepOrg` varchar(255) NOT NULL,
   `Event` varchar(255) NOT NULL,
   `Venue` varchar(255) NOT NULL,
   `Date` date NOT NULL,
   `TimeStart` time NOT NULL,
-  `TimeEnd` time NOT NULL,
-  PRIMARY KEY (`resID`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+  `TimeEnd` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `reservation`
@@ -119,6 +118,62 @@ CREATE TABLE IF NOT EXISTS `reservation` (
 INSERT INTO `reservation` (`resID`, `DepOrg`, `Event`, `Venue`, `Date`, `TimeStart`, `TimeEnd`) VALUES
 (1, 'PESO', 'Party', 'Plaza', '2019-02-02', '14:22:00', '14:22:00'),
 (2, 'JPIA', 'Party', 'Plaza', '2019-02-02', '14:22:00', '14:22:00');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `client`
+--
+ALTER TABLE `client`
+  ADD PRIMARY KEY (`ClientID`);
+
+--
+-- Indexes for table `event`
+--
+ALTER TABLE `event`
+  ADD PRIMARY KEY (`eventID`);
+
+--
+-- Indexes for table `facility`
+--
+ALTER TABLE `facility`
+  ADD PRIMARY KEY (`FacID`);
+
+--
+-- Indexes for table `reservation`
+--
+ALTER TABLE `reservation`
+  ADD PRIMARY KEY (`resID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `client`
+--
+ALTER TABLE `client`
+  MODIFY `ClientID` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=60;
+
+--
+-- AUTO_INCREMENT for table `event`
+--
+ALTER TABLE `event`
+  MODIFY `eventID` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `facility`
+--
+ALTER TABLE `facility`
+  MODIFY `FacID` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `reservation`
+--
+ALTER TABLE `reservation`
+  MODIFY `resID` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
