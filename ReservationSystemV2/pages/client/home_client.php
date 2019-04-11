6f3d784974cdf175a4b84340b54f44d26716f19a
JeronJavierto <37394437+JeronJavierto@users.noreply.github.com>
2019-04-11 20:00:54 +0800

    added calendar to home_client.php and calendar folder for home_client.php

--- ReservationSystemV2/pages/client/home_client.php
+++ ReservationSystemV2/pages/client/home_client.php
@@ -3,9 +3,10 @@
 <head>
 	<title>Home</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" type="text/css" href="../../stylesheet/style.css">
+	<link rel="stylesheet" type="text/css" href="../../stylesheet/style.css">	
 	<h1>FACILITY RESERVATION SYSTEM</h1>
-	<h3><a href="edit_profile.html" class="signup">Edit Profile</a></h3>
+	<h3><a href="edit_profile.html" class="signup">Edit Profile</a></h3><br>
+	<h3><a href="../../php/logout.php" class="signup">Log Out</a></h3>
 </head>
 <body>
 	<ul class="menu">
@@ -13,5 +14,7 @@
 		<li class="events"><a href="../../php/event_page.php" class="events">EVENTS</a></li>
 		<li class="faci"><a href="../../php/event_page.php" class="faci">FACILITIES</a></li>
 	</ul>
+
+	<?php include("homepage_client_calendar.php"); ?>
 </body>
 </html>
