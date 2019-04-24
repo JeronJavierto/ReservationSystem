f01d4ea54282eecd2e16d445b6182d3a663a31ac
JeronJavierto <37394437+JeronJavierto@users.noreply.github.com>
2019-04-24 20:37:10 +0800

    fixed the redirection for list_of_requests_admin.php

--- ReservationSystemV2/pages/admin/home_admin.php
+++ ReservationSystemV2/pages/admin/home_admin.php
@@ -19,7 +19,7 @@
 		<li class="events"><a href="../../php/event_page_admin.php" class="events">EVENTS</a></li>
 		<li class="faci"><a href="../../php/list_facilities_admin.php" class="faci">FACILITIES</a></li>
 		<li class="reser"><a href="list_of_reservations.php" class="reser">RESERVATION</a></li>
-		<li class="req"><a href="../../php/request_admin.php" class="req">REQUEST</a></li>
+		<li class="req"><a href="../../php/list_of_requests_admin.php" class="req">REQUEST</a></li>
 		<li class="rep"><a href="report.php" class="rep">REPORTS</a></li>
 	</ul>
 
