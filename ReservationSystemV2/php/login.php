1cd5c6061ff15fce32e243ce4c5e24ade15aa24e
JeronJavierto <37394437+JeronJavierto@users.noreply.github.com>
2019-04-24 20:34:59 +0800

    fixed the log in for office and admin

--- ReservationSystemV2/php/login.php
+++ ReservationSystemV2/php/login.php
@@ -9,12 +9,15 @@
       $myemail = mysqli_real_escape_string($conn,$_POST['email']);
       $mypassword = mysqli_real_escape_string($conn,$_POST['password']);      
       $mypassword = md5($mypassword);
+      $officeArray = array("SSD", "Student Affairs Office", "Security Office");
       
       $sql = "SELECT * FROM accounts WHERE email = '$myemail' and Password = '$mypassword'";
-
+      // $office = "SELECT Office FROM accounts WHERE email = '$myemail'";      
       $result = mysqli_query($conn,$sql);            
+      // $resultOffice = mysqli_query($conn,$office);
 
       $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+      // $rowOffice = mysqli_fetch_array($resultOffice,MYSQLI_ASSOC);
       // $active = $row['active'];
       
       $count = mysqli_num_rows($result);      
@@ -23,13 +26,18 @@
       if($count == 1) {
          $_SESSION['email'] = "email";
          $_SESSION['login_user'] = $myemail;
-
+      
          if($row['User_type'] == 'admin'){
-            header("location: ../pages/admin/home_admin.php");
+            for ($i=0; $i < 1; $i++){
+               if ($officeArray[$i] == $row['Office']){
+                  header("location: ../pages/office/home_office.php");
+               }else {
+                  header("location: ../pages/admin/home_admin.php");
+               }
+            }
          }else{
-            header("location: ../pages/client/home_client.php");
-         }
-
+               header("location: ../pages/client/home_client.php");
+            }
       }else {
          echo $error;
       }
