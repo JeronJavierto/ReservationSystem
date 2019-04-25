13db13b73f80e5ff03f743323c3ee96d44a8a8f4
JeronJavierto <37394437+JeronJavierto@users.noreply.github.com>
2019-04-26 01:45:07 +0800

    fixed: log in for offices

--- ReservationSystemV2/php/login.php
+++ ReservationSystemV2/php/login.php
@@ -12,32 +12,30 @@
       $officeArray = array("SSD", "Student Affairs Office", "Security Office");
       
       $sql = "SELECT * FROM accounts WHERE email = '$myemail' and Password = '$mypassword'";
-      // $office = "SELECT Office FROM accounts WHERE email = '$myemail'";      
-      $result = mysqli_query($conn,$sql);            
-      // $resultOffice = mysqli_query($conn,$office);
-
+      $notCus = "SELECT * FROM accounts WHERE Office NOT LIKE 'Custodian''s office' AND User_type LIKE 'admin'";
+      $result = mysqli_query($conn,$sql);
+      // $notCusResult = mysqli_query($conn,$notCus);
+      $notCusResult = mysqli_query($conn,$notCus);
       $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
-      // $rowOffice = mysqli_fetch_array($resultOffice,MYSQLI_ASSOC);
+      // $notCusRow = mysqli_fetch_array($notCusResult,MYSQLI_ASSOC);
       // $active = $row['active'];
-      
-      $count = mysqli_num_rows($result);      
+      // echo $notCusRow['Office'];
+      $count = mysqli_num_rows($result);
       // If result matched $myemail and $mypassword, table row must be 1 row
-
+      
       if($count == 1) {
          $_SESSION['email'] = "email";
          $_SESSION['login_user'] = $myemail;
       
          if($row['User_type'] == 'admin'){
-            for ($i=0; $i < 1; $i++){
-               if ($officeArray[$i] == $row['Office']){
-                  header("location: ../pages/office/home_office.php");
-               }else {
-                  header("location: ../pages/admin/home_admin.php");
-               }
-            }
+                  if ($row['Office'] == 'SAO' || $row['Office'] == 'SecOffice' || $row['Office'] == 'DeanOffice'){
+                     header("location: ../pages/office/home_office.php");
+                  }else {
+                     header("location: ../pages/admin/home_admin.php");
+                  }
          }else{
-               header("location: ../pages/client/home_client.php");
-            }
+            header("location: ../pages/client/home_client.php");
+         }
       }else {
          echo $error;
       }
