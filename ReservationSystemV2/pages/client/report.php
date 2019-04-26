59e44bf51fd7342853ee22f84c21ef2c75307101
2164165 <37785711+2164165@users.noreply.github.com>
2019-04-26 22:20:49 +0800

    report with navigation

--- ReservationSystemV2/pages/client/report.php
+++ ReservationSystemV2/pages/client/report.php
@@ -2,9 +2,18 @@
 <html>
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="../../stylesheet/style5.css">     
+<link rel="stylesheet" href="../../stylesheet/style5.css">
+<h1>FACILITY RESERVATION SYSTEM</h1>
+<h3><a href="edit_profile.html" class="signup">Edit Profile</a></h3><br>
+<h3><a href="../../php/logout.php" class="signup">Log Out</a></h3>
 </head>
     <body>
+<ul class="menu">
+		<li class="home"><a href="home_client.html" class="home">HOME</a></li>
+		<li class="events"><a href="../../php/event_page.php" class="events">EVENTS</a></li>
+		<li class="faci"><a href="../../php/list_facilities.php" class="faci">FACILITIES</a></li>
+        <li class="repo"><a href="../../php/Report.php" class="repo">REPORT</a></li>
+</ul>
     <div class="a">Reports</div>
         <div class="b">
             <div class="c">Incident Report</div>
