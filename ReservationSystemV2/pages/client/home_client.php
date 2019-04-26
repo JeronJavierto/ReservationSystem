b6365406fb58a313969827ce7bde7335841a05c1
2164165 <37785711+2164165@users.noreply.github.com>
2019-04-27 00:28:45 +0800

    more updated

--- ReservationSystemV2/pages/client/home_client.php
+++ ReservationSystemV2/pages/client/home_client.php
@@ -4,21 +4,21 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Home</title>
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" type="text/css" href="../../stylesheet/style.css">	
-	<h1>FACILITY RESERVATION SYSTEM</h1>
-	<h3><a href="edit_profile.html" class="signup">Edit Profile</a></h3><br>
-	<h3><a href="../../php/logout.php" class="signup">Log Out</a></h3>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="../../stylesheet/style5.css">
+<link rel="stylesheet" href="../../stylesheet/style.css">
+<h1>FACILITY RESERVATION SYSTEM</h1>
+<h3><a href="edit_profile.html" class="signup">Edit Profile</a></h3><br>
+<h3><a href="../../php/logout.php" class="signup">Log Out</a></h3>
 </head>
-<body>
-	<ul class="menu">
-		<li class="home"><a href="home_client.php" class="home">HOME</a></li>
-		<li class="events"><a href="../../php/event_page_client.php" class="events">EVENTS</a></li>
-		<li class="faci"><a href="../../php/list_facilities_client.php" class="faci">FACILITIES</a></li>
-        <li class="repo"><a href="../../php/Report.php" class="repo">REPORT</a></li>
+    <body>
         
-	</ul>
+<ul class="menu">
+		<li class="home"><a href="home_client.html" class="home">HOME</a></li>
+		<li class="events"><a href="../../php/event_page.php" class="events">EVENTS</a></li>
+		<li class="faci"><a href="../../php/list_facilities.php" class="faci">FACILITIES</a></li>
+        <li class="repo"><a href="../../php/Report.php" class="repo">REPORT</a></li>
+</ul>
 
 	<?php include("homepage_client_calendar.php"); ?>
 </body>
