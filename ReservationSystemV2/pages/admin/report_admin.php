44ea1101af878e5b7b30306b5dd376c6690efcc7
2164165 <37785711+2164165@users.noreply.github.com>
2019-04-27 16:34:01 +0800

    report_admin

---
+++ ReservationSystemV2/pages/admin/report_admin.php
@@ -0,0 +1,160 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="style5.css">
+<link rel="stylesheet" href="style6.css">
+<link rel="stylesheet" href="style.css">
+<h1>FACILITY RESERVATION SYSTEM</h1>
+<h3><a href="edit_profile.html" class="signup">Edit Profile</a></h3><br>
+<h3><a href="../../php/logout.php" class="signup">Log Out</a></h3>
+</head>
+    <body>
+        
+<ul class="menu">
+		<li class="home"><a href="home_client.html" class="home">HOME</a></li>
+		<li class="events"><a href="../../php/event_page.php" class="events">EVENTS</a></li>
+		<li class="faci"><a href="../../php/list_facilities.php" class="faci">FACILITIES</a></li>
+        <li class="repo"><a href="../../php/Report.php" class="repo">REPORT</a></li>
+</ul>
+
+
+<table id="customers">
+  <tr>
+    <th>Date of Occurence</th>
+    <th>Time of Occurence</th>
+    <th>Name</th>
+    <th>ID No.</th>
+    <th>Course</th>
+    <th>Description</th>
+    <th>Extend of Damage</th>
+    <th>Action Taken</th>
+    <th></th>
+  </tr>
+  <tr>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td><button class="button">Acknowledge</button></td>
+  </tr>
+  <tr>
+    <td></td>
+    <td></td>
+    <td></td>
+     <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+  <tr>
+    <td></td>
+    <td></td>
+    <td></td>
+     <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+  <tr>
+    <td></td>
+    <td></td>
+    <td></td>
+     <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+  <tr>
+    <td></td>
+    <td></td>
+    <td></td>
+     <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+  <tr>
+    <td></td>
+    <td></td>
+    <td></td>
+     <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+  <tr>
+    <td></td>
+    <td></td>
+    <td></td>
+     <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+  <tr>
+    <td></td>
+    <td></td>
+    <td></td>
+     <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+  <tr>
+    <td></td>
+    <td></td>
+    <td></td>
+     <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+  <tr>
+    <td></td>
+    <td></td>
+    <td></td>
+     <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+  <tr>
+   <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+     <td></td>
+    <td></td>
+    <td></td>
+   
+  </tr>
+</table>
+
+</body>
+</html>
