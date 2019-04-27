330387924225c2eb58a208a964663d81a7f7d359
lesterdeguzman <32890425+lesterdeguzman@users.noreply.github.com>
2019-04-27 16:10:19 +0800

    Add files via upload
    updated list facilities home

--- ReservationSystemV2/php/list_facilities_home.php
+++ ReservationSystemV2/php/list_facilities_home.php
@@ -1,52 +1,146 @@
-<?php include('DBConnector.php'); ?>
 <!DOCTYPE html>
 <html>
 <head>
   <title>Facilities</title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" type="text/css" href="../stylesheet/style.css">
+  <link rel="stylesheet" href="../stylesheet/bootstrap.min.css">
   <h1>FACILITY RESERVATION SYSTEM</h1>
 </head>
 
 <body>
-  <ul class="menu"> 
+
+<ul class="menu"> 
     <li class="home_index"><a href="../index.html" class="home">HOME</a></li>
     <li class="events_index"><a href="event_page_home.php" class="events">EVENTS</a></li>
     <li class="faci_index"><a href="list_facilities_home.php" class="faci">FACILITIES</a></li>
   </ul>
 
-    <table id="customers">
-      <tr>
-        <th>Level</th>
-        <th>Room</th>
-        <th>Room Type</th>
-        <th>Description</th>
-        <th>Capacity</th>     
-      </tr>
-  <?php
-    $sql = "SELECT * FROM facility";
-    $result = $conn-> query($sql);
+<div style="width:700px; margin:0 auto;">
+<br>
+<table class="table table-striped table-bordered">
+<thead>
+<tr>
+<th>Level</th>
+<th>Room</th>
+<th>Room Type</th>
+<th>Description</th>
+</tr>
+</thead>
+<tbody>
+<?php
+include('DBconnector.php');
 
-    if ($result-> num_rows > 0){
-      while ($row = $result-> fetch_assoc()){
-        echo "<tr>
-            <td>" . $row["Level"] . "</td>
-            <td>" . $row["room"] . "</td>
-            <td>" . $row["roomType"] . "</td>
-            <td>"  . $row["description"] . "</td>
-            <td>" . $row["capacity"] . "</td>
-            </tr>";
-      }
-      echo "</table>";
-    }else{
-      echo "0 result";
-    }
+if (isset($_GET['page_no']) && $_GET['page_no']!="") {
+	$page_no = $_GET['page_no'];
+	} else {
+		$page_no = 1;
+        }
 
-    $conn-> close();
-  ?>    
-    </table>
+	$total_records_per_page = 10;
+    $offset = ($page_no-1) * $total_records_per_page;
+	$previous_page = $page_no - 1;
+	$next_page = $page_no + 1;
+	$adjacents = "2"; 
 
-  <button onclick="document.getElementById('id01').style.display='block'" style="width:auto;" class="loginB">LOGIN</button>
+	$result_count = mysqli_query($conn,"SELECT COUNT(*) As total_records FROM `facility`");
+	$total_records = mysqli_fetch_array($result_count);
+	$total_records = $total_records['total_records'];
+    $total_no_of_pages = ceil($total_records / $total_records_per_page);
+	$seconnd_last = $total_no_of_pages - 1; // total page minus 1
+
+    $result = mysqli_query($conn,"SELECT * FROM `facility` LIMIT $offset, $total_records_per_page");
+    while($row = mysqli_fetch_array($result)){
+		echo "<tr>
+			  <td>".$row['Level']."</td>
+			  <td>".$row['room']."</td>
+	 		  <td>".$row['roomType']."</td>
+		   	  <td>".$row['description']."</td>
+		   	  </tr>";
+        }
+	mysqli_close($conn);
+    ?>
+</tbody>
+</table>
+
+<div style='padding: 10px 20px 0px; border-top: dotted 1px #CCC;'>
+<strong>Page <?php echo $page_no." of ".$total_no_of_pages; ?></strong>
+</div>
+
+<ul class="pagination">
+	<?php // if($page_no > 1){ echo "<li><a href='?page_no=1'>First Page</a></li>"; } ?>
+    
+	<li <?php if($page_no <= 1){ echo "class='disabled'"; } ?>>
+	<a <?php if($page_no > 1){ echo "href='?page_no=$previous_page'"; } ?>>Previous</a>
+	</li>
+       
+    <?php 
+	if ($total_no_of_pages <= 10){  	 
+		for ($counter = 1; $counter <= $total_no_of_pages; $counter++){
+			if ($counter == $page_no) {
+		   echo "<li class='active'><a>$counter</a></li>";	
+				}else{
+           echo "<li><a href='?page_no=$counter'>$counter</a></li>";
+				}
+        }
+	}
+	elseif($total_no_of_pages > 10){
+		
+	if($page_no <= 4) {			
+	 for ($counter = 1; $counter < 8; $counter++){		 
+			if ($counter == $page_no) {
+		   echo "<li class='active'><a>$counter</a></li>";	
+				}else{
+           echo "<li><a href='?page_no=$counter'>$counter</a></li>";
+				}
+        }
+		echo "<li><a>...</a></li>";
+		echo "<li><a href='?page_no=$seconnd_last'>$seconnd_last</a></li>";
+		echo "<li><a href='?page_no=$total_no_of_pages'>$total_no_of_pages</a></li>";
+		}
+
+	 elseif($page_no > 4 && $page_no < $total_no_of_pages - 4) {		 
+		echo "<li><a href='?page_no=1'>1</a></li>";
+		echo "<li><a href='?page_no=2'>2</a></li>";
+        echo "<li><a>...</a></li>";
+        for ($counter = $page_no - $adjacents; $counter <= $page_no + $adjacents; $counter++) {			
+           if ($counter == $page_no) {
+		   echo "<li class='active'><a>$counter</a></li>";	
+				}else{
+           echo "<li><a href='?page_no=$counter'>$counter</a></li>";
+				}                  
+       }
+       echo "<li><a>...</a></li>";
+	   echo "<li><a href='?page_no=$seconnd_last'>$seconnd_last</a></li>";
+	   echo "<li><a href='?page_no=$total_no_of_pages'>$total_no_of_pages</a></li>";      
+            }
+		
+		else {
+        echo "<li><a href='?page_no=1'>1</a></li>";
+		echo "<li><a href='?page_no=2'>2</a></li>";
+        echo "<li><a>...</a></li>";
+
+        for ($counter = $total_no_of_pages - 6; $counter <= $total_no_of_pages; $counter++) {
+          if ($counter == $page_no) {
+		   echo "<li class='active'><a>$counter</a></li>";	
+				}else{
+           echo "<li><a href='?page_no=$counter'>$counter</a></li>";
+				}                   
+                }
+            }
+	}
+?>
+    
+	<li <?php if($page_no >= $total_no_of_pages){ echo "class='disabled'"; } ?>>
+	<a <?php if($page_no < $total_no_of_pages) { echo "href='?page_no=$next_page'"; } ?>>Next</a>
+	</li>
+    <?php if($page_no < $total_no_of_pages){
+		echo "<li><a href='?page_no=$total_no_of_pages'>Last &rsaquo;&rsaquo;</a></li>";
+		} ?>
+</ul>
+</div>
+
+<button onclick="document.getElementById('id01').style.display='block'" style="width:auto;" class="loginB">LOGIN</button>
   <button onclick="document.getElementById('id02').style.display='block'" style="width:auto;" class="signupB">SIGN UP</button>
 
   <div id="id01" class="loginmodal">
@@ -151,6 +245,6 @@
       	</div>
       	</form>
       </div>
-	
+
 </body>
 </html>
