c5b1753a1c1d418b083aaf237cab4e0571645659
2164165 <37785711+2164165@users.noreply.github.com>
2019-04-28 00:42:38 +0800

    updated report

--- ReservationSystemV2/php/Report.php
+++ ReservationSystemV2/php/Report.php
@@ -2,38 +2,42 @@
 <html>
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="../../stylesheet/style5.css">      
-<h1>FACILITY RESERVATION SYSTEM</h1>
-<h3><a href="../../php/logout.php" class="signup">Log Out</a></h3>
+<link rel="stylesheet" href="style5.css">
+<link rel="stylesheet" href="style.css">   
+	<title>List of Facilities</title>
+	<h1>FACILITY RESERVATION SYSTEM</h1>
+	<h3><a href="edit_profile.php" class="signup">Edit Profile</a></h3><br>
+	<h3><a href="logout.php" class="signup">Log Out</a></h3>
 </head>
     <body>
-        
+    
 <ul class="menu">
-		<li class="home"><a href="home_client.html" class="home">HOME</a></li>
-		<li class="events"><a href="../../php/event_page.php" class="events">EVENTS</a></li>
-		<li class="faci"><a href="../../php/list_facilities.php" class="faci">FACILITIES</a></li>
+	    <li class="home"><a href="../pages/client/home_client.php" class="home">HOME</a></li>
+	    <li class="events"><a href="event_page_client.php" class="events">EVENTS</a></li>
+	    <li class="faci"><a href="list_facilities_client.php" class="faci">FACILITIES</a></li>
         <li class="repo"><a href="../../php/Report.php" class="repo">REPORT</a></li>
-</ul>
-    <div class="a">Reports</div>
-        <div class="b">
-            <div class="c">Incident Report</div>
+  </ul>
+        
+    <div class="abc">Reports</div>
+        <div class="bcd">
+            <div class="cde">Incident Report</div>
         <br>
 <form>
     <label for="Org">Date of Occurence</label>
-    <input type="text" id="Occur" name="date/occur" placeholder="....">
+    <input type="fill" id="Occur" name="date/occur" placeholder="....">
     <br>
     <label for="Vent">Time of Occurence</label>
-    <input type="text" id="" time/occur="Evnt" placeholder="... ">
+    <input type="fill" id="" time/occur="Evnt" placeholder="... ">
     <br>
     <h4>Causet of Incident</h4>
     <label for="Ven">Name</label>
-    <input type="text" id="n" name="nm" placeholder=".....">
+    <input type="fill" id="n" name="nm" placeholder=".....">
     <br>
     <label for="Dat">ID no.</label>
-    <input type="text" id="no" name="idno" placeholder="...... ">
+    <input type="fill" id="no" name="idno" placeholder="...... ">
     <br>
     <label for="Tim">Course</label>
-    <input type="text" id="cr" name="course" placeholder=".....">
+    <input type="fill" id="cr" name="course" placeholder=".....">
     <br>
     <h4>Description</h4>
     <textarea rows="4" cols="50" name="comment">
@@ -47,7 +51,7 @@
     <br>
     <br>
  
-    <button class="button">Acknowledge</button>
+    <button class="butt">Acknowledge</button>
         
 </form>
 
